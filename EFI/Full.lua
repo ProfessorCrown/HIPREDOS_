2acaa19039c29e2dd664d33abc2362356084192f
Professor Crown <44137797+ProfessorCrown@users.noreply.github.com>
2020-02-12 00:08:26 +0900

    gi

--- EFI/Full.lua
+++ EFI/Full.lua
@@ -269,7 +269,7 @@ while uptime() < deadline do
 
 		if internetAddress then	
 			tableInsert(utilities, 2, menuElement("Internet recovery", function()
-				local handle, data, result, reason = componentProxy(internetAddress).request("https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Installer/Main.lua"), ""
+				local handle, data, result, reason = componentProxy(internetAddress).request("https://raw.githubusercontent.com/ProfessorCrown/HIPREDOS_/master/Installer/Main.lua"), ""
 
 				if handle then
 					status(stringsMain, "Downloading recovery script")
