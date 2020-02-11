1e248b83743ac53c965bc41fdbd9a11c7320cb43
Foroney <44137797+ProfessorCrown@users.noreply.github.com>
2020-02-11 20:52:55 +0900

    Update Main.lua

--- Installer/Main.lua
+++ Installer/Main.lua
@@ -1,7 +1,7 @@
 
 local EEPROMProxy, internetProxy, GPUProxy = component.proxy(component.list("eeprom")()), component.proxy(component.list("internet")()), component.proxy(component.list("gpu")())
 
-local repositoryURL = "https://raw.githubusercontent.com/ProfessorCrown/HIPREDOS/master/"
+local repositoryURL = "https://raw.githubusercontent.com/ProfessorCrown/HIPREDOS_/master/"
 local installerURL = "Installer/"
 local EFIURL = "EFI/Minified.lua"
 
