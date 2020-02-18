8d552e0a016440f5d34e90034888b991ba387d41
Professor Crown <44137797+ProfessorCrown@users.noreply.github.com>
2020-02-18 18:41:31 +0900

    Update GUI.lua

--- Libraries/GUI.lua
+++ Libraries/GUI.lua
@@ -866,7 +866,7 @@ function GUI.actionButtons(x, y, fatSymbol)
 	local maximize1 = fatSymbol and "██" or "██"
 	
 	local container = GUI.container(x, y, 9, 1)
-	container.close = container:addChild(GUI.button(1, 1, 1, 1, nil, 0xFF4940, nil, 0x992400, close1))
+	container.close = container:addChild(GUI.button(2, 1, 1, 1, nil, 0xFF4940, nil, 0x992400, close1))
 	container.minimize = container:addChild(GUI.button(8, 1, 1, 1, nil, 0xFFB640, nil, 0x996D00, minimize1))
 	container.maximize = container:addChild(GUI.button(5, 1, 1, 1, nil, 0x00B640, nil, 0x006D40, maximize1))
 
